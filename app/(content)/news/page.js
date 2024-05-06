296f800c0b18b8d0ebc36f56d43de9c4bdf35df4
ryoheiY <momoyamashia@icloud.com>
2024-05-06 19:02:48 +0900

    バックエンドからのデータ取得修正版

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,44 +1,16 @@
-"use client"
-
-import { DUMMY_NEWS } from "@/components/dummy-news";
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
+export default async function NewsPage() {
+  const res = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    async function fetchNews(){
-      const res = await fetch("http://localhost:8080/news");
-      if(!res.ok){
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      }
-      const resData = await res.json();
-      setIsLoading(false);
-      setNews(resData);
-
-    }
-    fetchNews();
-    
-  }, []);
-
-  if(isLoading){
-    return <p>Loading</p>
-  }
-  if(error){
-    return <p>{error}</p>
-  }
-  let newsContent;
-  if(news){
-    newsContent =  <NewsList newsList={news} />;
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
 
+  const news = await res.json();
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList newsList={news} />
     </>
   );
 }
